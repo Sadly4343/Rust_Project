8a5fe3747e815101ae5fd221188687f8f410fc76
Sadly4343 <loganhartshorn@gmail.com>
2024-12-03 04:46:12 -0700

    co

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,18 @@ fn main() {
     loop {
         //Print Line Function to Display to User
         println!("Enter your calculation or type Exit to leave.");
-        //
+        //Allows display above to return immediately rather than later.
         io::stdout().flush().unwrap();
-
+        //Input is set to be changeable.
         let mut input = String::new();
+        //Console Write
         io::stdin().read_line(&mut input).unwrap();
 
-        let input = input.trim();
+        let input = input.trim().parse::<i32>();
 
-        println!("You entered: {}", input);
+        match input {
+            Ok(num) => println!("Youve entered: {}", num),
+            Err(_) => println!("That is not a valid number!"),
+        }
     }
 }
